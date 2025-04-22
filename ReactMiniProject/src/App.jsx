40463fb53bc879465eaf8d93cc55449979df11d0
Varsha Bhagat <Varsha.Bhagat@vdx.tv>
2025-04-22 18:12:29 +0530

    gallery

--- ReactMiniProject/src/App.jsx
+++ ReactMiniProject/src/App.jsx
@@ -1,6 +1,5 @@
 // import Counter from "./mini-project/Counter";
 import ImageGallery from "./mini-project/Image-gallery/index"
-import "./mini-project/Image-gallery/index.css"
 import "./App.css";
 
 function App() {
